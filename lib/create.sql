3a8fe22c7fb910dc74cfce3f06c6eb8cb9bbfb0f
Melissa Guachun <1212malka@gmail.com>
2021-03-11 22:25:25 -0500

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,10 @@
+
 CREATE TABLE bears (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    age INTEGER,
-    gender CHAR(1),
-    color TEXT,
-    temperament TEXT,
-    alive BOOLEAN
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender CHAR(1),
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
 );
